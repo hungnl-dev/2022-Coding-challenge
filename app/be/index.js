038cfc8537e2a295dc4bf3230b8fc8a922274cfa
Luke Nguyen <luke.nguyen@theaccessgroup.com>
2022-08-27 10:54:28 +0700

    Mock API

--- app/be/index.js
+++ app/be/index.js
@@ -2,7 +2,41 @@ const express = require('express')
 const app = express()
 const port = 9000
 
-app.get('/', (req, res) => {
+app.get('/api/v1/state', (req, res) => {
+  const responseData = {
+    data: {
+      incidents: [
+        {
+          id: 1,
+          codeName: "Incident A",
+          loc: {
+            x: 18,
+            y: 8
+          },
+          officerId: 1
+        }
+      ],
+      officers: [
+        {
+          id: 1,
+          badgeName: "Minh 1",
+          loc: {
+            x: 50,
+            y: 35
+          }
+        },
+        {
+          id: 2,
+          badgeName: "Minh 2",
+          loc: {
+            x: 10,
+            y: 20
+          }
+        }
+      ]
+    },
+    error: null
+  }
   res.send('Hello World!')
 })
 
